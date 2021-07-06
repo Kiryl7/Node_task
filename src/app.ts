b91041d02892b544e965a6a73e946f6447f00b50
Kiryl <Kiryl7St@gmail.com>
2021-07-06 16:32:24 +0300

    refactor code with prettier

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express from 'express'
-import { tasks as taskRoute } from './tasks/tasks.conroller'
-import { ErrorHandler, handleError } from "./helpers/error"
+import express from 'express';
+import { tasks as taskRoute } from './tasks/tasks.conroller';
+import { ErrorHandler, handleError } from './helpers/error';
 
-export const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+export const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/error', (req, res) => {
-    throw new ErrorHandler(500, 'Internal server error');
-})
+  throw new ErrorHandler(500, 'Internal server error');
+});
 
-app.use('/tasks', taskRoute)
+app.use('/tasks', taskRoute);
 
 app.use((err, req, res, next) => {
-    handleError(err, res)
-})
+  handleError(err, res);
+});
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,54 +1,54 @@
-import { getAll, getById, delById, update, save } from "./tasks.repository"
-import { Task } from "./tasks.repository"
+import { getAll, getById, delById, update, save } from './tasks.repository';
+import { Task } from './tasks.repository';
 
-const getTasks = async (): Promise <Array<Task> | null> => {
-    try {
-        return await getAll()
-    } catch (error) {
-        if (error) {
-            return error.message
-        }
+const getTasks = async (): Promise<Array<Task> | null> => {
+  try {
+    return await getAll();
+  } catch (error) {
+    if (error) {
+      return error.message;
     }
-}
+  }
+};
 
 const getOneTask = async (id: number): Promise<Array<Task> | null> => {
-    try {
-        return await getById(id)
-    } catch (error) {
-        if (error) {
-            return error.message
-        }
+  try {
+    return await getById(id);
+  } catch (error) {
+    if (error) {
+      return error.message;
     }
-}
+  }
+};
 
 const saveTask = async (task: Task): Promise<Task> => {
-    try {
-        return await save(task)
-    } catch(error) {
-        if (error) {
-            return error.message
-        }
+  try {
+    return await save(task);
+  } catch (error) {
+    if (error) {
+      return error.message;
     }
-}
+  }
+};
 
 const delTask = async (id: number): Promise<Task> => {
-    try {
-        return await delById(id)
-    } catch (error) {
-        if (error) {
-            return error.message
-        }
+  try {
+    return await delById(id);
+  } catch (error) {
+    if (error) {
+      return error.message;
     }
-}
+  }
+};
 
 const updateTask = async (id: number, task: Task): Promise<Task> => {
-    try {
-        return await update(id, task)
-    } catch (error) {
-        if (error) {
-            return error.message
-        }
+  try {
+    return await update(id, task);
+  } catch (error) {
+    if (error) {
+      return error.message;
     }
-}
+  }
+};
 
-export { getOneTask, saveTask, delTask, updateTask, getTasks }
+export { getOneTask, saveTask, delTask, updateTask, getTasks };
