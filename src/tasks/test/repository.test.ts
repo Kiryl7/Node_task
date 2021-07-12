bf9f7c47b9847fe5bb5853a95f8baa671a61b46e
Kiryl <Kiryl7St@gmail.com>
2021-07-12 18:30:15 +0300

    add test to getById method

--- src/tasks/test/repository.test.ts
+++ src/tasks/test/repository.test.ts
@@ -8,7 +8,7 @@ jest.mock('pg', () => {
   return { Pool: jest.fn(() => mockPool) }
 })
 
-describe('getAll', () => {
+describe('repository methods', () => {
   let pool
   beforeEach(() => {
     pool = new Pool()
@@ -22,5 +22,18 @@ describe('getAll', () => {
     const expected = await getAll()
     expect(pool.query).toBeCalled()
     expect(expected).toEqual(mockTasks)
+  })
+  test('should return one task', async () => {
+    pool.query.mockResolvedValue({ rowCountL: 0, rows: [] })
+    const expected = await getById(1)
+    expect(pool.query).toBeCalled()
+    expect(expected).toEqual(mockTasks)
+  })
+  test('should delete task by ID', async () => { 
+      const client = await pool.connect()
+      pool.query.mockResolvedValue({rowCount: 0, rows: [] }) //fix pool.query to pool.client
+      const expected = await delById(1)
+      expect(pool.query).toBeCalled()
+      expect(expected).toEqual(mockTasks)
   })
 })
